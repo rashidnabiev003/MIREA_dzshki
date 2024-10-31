28210ed12efdd7fe39715a1e953ad442be9c0f6b
Rashid Nabiev <rashidkhonnabiev@gmail.com>
2024-11-01 00:34:49 +0300

    backprop train

--- amrs_labs/amrs_lab_1_nabiev.py
+++ amrs_labs/amrs_lab_1_nabiev.py
@@ -2,74 +2,83 @@ import numpy as np
 import pygame, sys
 import time
 import matplotlib.pyplot as plt
-from PIL.ImageColor import colormap
 
 WIDTH = 1000
 HEIGHT = 800
 
+
 def dist(p1, p2):
-    dx=p2[0]-p1[0]
-    dy=p2[1]-p1[1]
-    return np.sqrt(dx*dx+dy*dy)
+    dx = p2[0] - p1[0]
+    dy = p2[1] - p1[1]
+    return np.sqrt(dx * dx + dy * dy)
+
 
 class Obj:
     def __init__(self, x, y, color):
-        self.x=x
-        self.y=y
-        self.color=color
-        self.reservedRobot=None
-        self.finished=False
+        self.x = x
+        self.y = y
+        self.color = color
+        self.reservedRobot = None
+        self.finished = False
+
     def getPos(self):
         return self.x, self.y
+
     def draw(self, screen):
-        r=10
+        r = 10
         pygame.draw.ellipse(screen, self.color,
-    [self.x-r, self.y-r, 2*r, 2*r], 2)
+                            [self.x - r, self.y - r, 2 * r, 2 * r], 2)
 
 
 class Robot:
     def __init__(self, x, y):
-        self.x=x
-        self.y=y
-        self.attachedObj=None
-        self.target=None
+        self.x = x
+        self.y = y
+        self.attachedObj = None
+        self.target = None
+
     def getPos(self):
         return self.x, self.y
+
     def draw(self, screen):
-        r=20
+        r = 20
         pygame.draw.ellipse(screen, (255, 0, 0),
-    [self.x-r, self.y-r, 2*r, 2*r], 2)
+                            [self.x - r, self.y - r, 2 * r, 2 * r], 2)
+
     def simulate(self):
         if self.target is not None:
-            p1=self.target
-            p2=(self.x, self.y)
-            v=np.array(p1)-np.array(p2)
-            d=dist(p1, p2)
-            if d>0:
-                v=v/d
-                self.x+=v[0]*5
-                self.y+=v[1]*5
+            p1 = self.target
+            p2 = (self.x, self.y)
+            v = np.array(p1) - np.array(p2)
+            d = dist(p1, p2)
+            if d > 0:
+                v = v / d
+                self.x += v[0] * 20
+                self.y += v[1] * 20
         if self.attachedObj is not None:
-            self.attachedObj.x=self.x
-            self.attachedObj.y=self.y
+            self.attachedObj.x = self.x
+            self.attachedObj.y = self.y
+
     def findNearestObj(self, objs, threshold=100500):
-        res=None
-        D=100500
+        res = None
+        D = 100500
         for o in objs:
-            if o.reservedRobot is not None and o.reservedRobot!=self:
+            if o.reservedRobot is not None and o.reservedRobot != self:
                 continue
             if o.finished:
                 continue
-            dNew=dist(o.getPos(), self.getPos())
-            if dNew<D:
-                D=dNew
-                res=o
-        if D>threshold:
-            res=None
+            dNew = dist(o.getPos(), self.getPos())
+            if dNew < D:
+                D = dNew
+                res = o
+        if D > threshold:
+            res = None
         return res
+
     def take(self, obj):
         if obj is not None:
-            self.attachedObj=obj
+            self.attachedObj = obj
+
 
 def distributeTasks(robots, objs, goal):
     for r in robots:
@@ -94,6 +103,7 @@ def distributeTasks(robots, objs, goal):
             r.target = obj.getPos()
             obj.reservedRobot = r
 
+
 def checkMission(robots, objs, goal):
     for r in robots:
         if dist(r.getPos(), goal.getPos()) > 20:
@@ -103,27 +113,29 @@ def checkMission(robots, objs, goal):
             return False
     return True
 
+
 def generateObjects(N):
-    res=[]
+    res = []
     for i in range(N):
-        o=Obj(np.random.randint(50, WIDTH-50),
-        np.random.randint(50, HEIGHT-50),(0,255,0))
+        o = Obj(np.random.randint(50, WIDTH - 50),
+                np.random.randint(50, HEIGHT - 50), (0, 255, 0))
         res.append(o)
     return res
 
+
 Robots = [
-        Robot(150, 150),
-        Robot(250, 250),
-        Robot(350, 350),
-        Robot(450, 450),
-        Robot(550, 550)
-        ]
+    Robot(150, 150),
+    Robot(250, 250),
+    Robot(350, 350),
+    Robot(450, 450),
+    Robot(550, 550)
+]
+
 
 def main(num_robots, num_objs):
     pygame.init()
     screen = pygame.display.set_mode((WIDTH, HEIGHT))
     start = time.time()
-
 
     objs = generateObjects(num_objs)
     goal = Obj(750, 450, (0, 0, 255))
@@ -154,6 +166,7 @@ def main(num_robots, num_objs):
 
     end = time.time()
     return end - start
+
 
 n_robots = [1, 2, 3, 4, 5]
 n_objs = [5, 5, 10, 10, 15]
